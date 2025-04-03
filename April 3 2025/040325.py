a0da9815a0aba39b21bd588b6f37a57402e9cd24
frazer1449 <frazer1449@gmail.com>
2025-04-03 23:41:56 +0900

    April 3, 2025

---
+++ April 3 2025/040325.py
@@ -0,0 +1,44 @@
+# Topic: Sliding Window, Level: Hard
+
+# You are given an array of integers nums and an integer k. There is a sliding window of size k that starts at the left edge of the array. The window slides one position to the right until it reaches the right edge of the array.
+# Return a list that contains the maximum element in the window at each step.
+
+# Example: 
+# Input: nums = [1,2,1,0,4,2,6], k = 3
+
+# Output: [2,2,4,4,6]
+
+# Explanation: 
+# Window position            Max
+# ---------------           -----
+# [1  2  1] 0  4  2  6        2
+#  1 [2  1  0] 4  2  6        2
+#  1  2 [1  0  4] 2  6        4
+#  1  2  1 [0  4  2] 6        4
+#  1  2  1  0 [4  2  6]       6
+
+import collections
+from typing import List
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = collections.deque()
+        l = r = 0
+
+        while r < len(nums):
+            # pop smaller values from q
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            # remove left val from window
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+        
+        return output
