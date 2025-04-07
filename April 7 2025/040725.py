6e7c4d31dd59d831e938c5b7d89ab4c3d2533b6a
frazer1449 <frazer1449@gmail.com>
2025-04-07 23:46:53 +0900

    April 7, 2025

---
+++ April 7 2025/040725.py
@@ -0,0 +1,26 @@
+# Topic: Bit Manipulation, Level: Easy
+
+# You are given a non-empty array of integers nums. Every integer appears twice except for one.
+# Return the integer that appears only once.
+# You must implement a solution with O(n) runtime complexity and use only O(1) extra space.
+
+# Hint: Two Solutions. One uses O(n) memory and the other uses O(1) memory. 
+
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashMap = {}
+        for num in nums:
+            hashMap[num] = hashMap.get(num, 0) + 1
+            if hashMap[num] == 2:
+                hashMap.pop(num)
+        
+        return list(hashMap.keys())[0]
+    
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0 # store XOR result
+        for n in nums:
+            res = n ^ res
+        return res
+    
