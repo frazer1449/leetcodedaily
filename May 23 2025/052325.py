bd6745df12594f999f05837f13c8aa60241b1967
frazer1449 <frazer1449@gmail.com>
2025-05-23 22:33:13 +0900

    Last Stone Weight

---
+++ May 23 2025/052325.py
@@ -0,0 +1,23 @@
+import heapq
+from typing import List
+# Last Stone Weight
+
+# heapify(array): creates a minHeap for the array
+# heappop():
+# 1. pops upmost element 
+# 2. puts minHeap[-1] into the top of array 
+# 3. shifts down so that heap property is satisfied
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-stone for stone in stones]
+        heapq.heapify(stones)
+
+        while (len(stones) > 1):
+            y = -heapq.heappop(stones)
+            x = -heapq.heappop(stones)
+            if (y > x):
+                heapq.heappush(stones, x - y)
+
+        return (0 if len(stones) == 0 else -stones[0])
+
