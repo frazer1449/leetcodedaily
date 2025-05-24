e3d1b6c26d38de938c1ffdc2ceca9ff8cd48503c
frazer1449 <frazer1449@gmail.com>
2025-05-24 23:03:42 +0900

    May 24, 2025: K Closest Points to Origin

---
+++ May 24 2025/052425.py
@@ -0,0 +1,20 @@
+import heapq
+from typing import List
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        ans = []
+
+        for x, y in points:
+            dist = x**2 + y**2
+            minHeap.append([dist, x, y])
+        
+        heapq.heapify(minHeap)
+        for i in range(k):
+            ans.append(heapq.heappop(minHeap)[1:3])
+
+        return ans
+        
+
+
