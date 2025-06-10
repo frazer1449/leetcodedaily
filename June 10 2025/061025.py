689b2dc250877df00aafa1ba32bd22e81595a8f6
frazer1449 <frazer1449@gmail.com>
2025-06-10 09:42:17 +0900

    June 10, 2025: Remove Duplicates In Place

---
+++ June 10 2025/061025.py
@@ -0,0 +1,36 @@
+# Two Pointer
+# Remove Duplicates From Sorted Array
+
+# You are given an integer array nums sorted in non-decreasing order. Your task is to remove duplicates from nums in-place so that each element appears only once.
+
+# After removing the duplicates, return the number of unique elements, denoted as k, such that the first k elements of nums contain the unique elements.
+
+# Note:
+
+# The order of the unique elements should remain the same as in the original array.
+# It is not necessary to consider elements beyond the first k positions of the array.
+# To be accepted, the first k elements of nums must contain all the unique elements.
+# Return k as the final result.
+
+# [1, 2, 3, 3, 3, 4, 4]
+
+from typing import List
+
+class Solution:
+    @staticmethod
+    def removeDuplicates(nums: List[int]) -> int:
+        n = len(nums)
+        l, r = 0, 0
+        while r < n:
+            if nums[l] != nums[r]:
+                l+=1
+                nums[l] = nums[r]
+            else:
+                r+=1
+        return l+1
+    
+
+print(Solution.removeDuplicates([1, 2, 3, 3, 3, 4, 4, 4, 5, 6, 6, 7, 7, 8]))
+         
+
+            
