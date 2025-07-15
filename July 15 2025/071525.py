d610c52323dab0b2de3bd216094b0b5a6c391019
frazer1449 <frazer1449@gmail.com>
2025-07-15 19:05:10 +0900

    July 15, 2025: LCS

---
+++ July 15 2025/071525.py
@@ -0,0 +1,19 @@
+# Longest Common Subsequence (Recursion + 2-DP)
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # chars of text1: rows, chars of text2: cols
+        memo = {}
+
+        def dfs(i, j):
+            if i == len(text1) or j == len(text2):
+                return 0
+            if (i, j) in memo:
+                return memo[(i,j)]
+            if text1[i] == text2[j]:
+                memo[(i,j)] = 1 + dfs(i+1, j+1)
+            else:
+                memo[(i,j)] = max(dfs(i, j+1), dfs(i+1, j))
+            return memo[(i, j)]
+        
+        return dfs(0, 0)
